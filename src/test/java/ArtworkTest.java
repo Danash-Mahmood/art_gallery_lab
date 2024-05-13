881cb218c2c31c80f2cf2071ff3933819a484b83
Sabah Yusuf <sabah.yusuf@outlook.com>
2024-05-13 15:04:35 +0100

    updated git ignore

---
+++ src/test/java/ArtworkTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class ArtworkTest {
+
+    Artwork painting;
+    Artist artist;
+
+    @BeforeEach
+
+
+
+
+
+
+}
