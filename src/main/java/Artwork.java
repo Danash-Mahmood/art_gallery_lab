649d117035ec8ec0fe2b65f2c2c863759bcbba3c
Danash Mahmood <danash.mahmoodof@gmail.com>
2024-05-13 15:36:10 +0100

    addding the Artwork class and associated methods MVP

--- src/main/java/Artwork.java
+++ src/main/java/Artwork.java
@@ -1,5 +1,41 @@
 public class Artwork {
 
+    private String title;
+    private Artist artist; // Why don't we need to do Artist artist = new Artist();
+    private int price;
+
+    public Artwork(String inputTitle,int inputPrice, Artist inputArtist){
+
+        this.title = inputTitle;
+        this.price = inputPrice;
+        this.artist = inputArtist;
+
+
+
+    }
+
+
+    public String getTitle(){
+        return this.title;
+    }
+
+    public void setTitle(String inputTitle){
+        this.title = inputTitle;
+    }
+
+    public String getArtist(){
+        return this.artist.getName();
+    }
+
+    public int getPrice(){
+        return this.price;
+    }
+
+    public void setPrice(int amount){
+        this.price = amount;
+    }
+
+
 
 
 
--- src/test/java/ArtworkTest.java
+++ src/test/java/ArtworkTest.java
@@ -10,6 +10,55 @@ public class ArtworkTest {
 
     @BeforeEach
 
+    public void setUp(){
+        artist = new Artist("Da Vinci");
+        painting = new Artwork("Mona Lisa",1000, artist);
+    }
+
+    @Test
+
+    public void checkGetTitle(){
+        assertThat(painting.getTitle()).isEqualTo("Mona Lisa");
+    }
+
+    @Test
+
+    public void checkSetTitle(){
+        painting.setTitle("The Starry Night");
+        assertThat(painting.getTitle()).isEqualTo("The Starry Night");
+    }
+
+    @Test
+
+    public void checkGetArtist(){
+        assertThat(painting.getArtist()).isEqualTo(artist.getName());
+
+    }
+
+    @Test
+
+    public void checkGetPrice(){
+        assertThat(painting.getPrice()).isEqualTo(1000);
+    }
+
+    @Test
+
+    public void checkSetPrice(){
+        painting.setPrice(900);
+        assertThat(painting.getPrice()).isEqualTo(900);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
